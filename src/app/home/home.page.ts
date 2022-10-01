6479469cd96461ce8b2a6631d59e24e53798eeea
Natalia CM <mnacamu@gmail.com>
2022-10-01 19:53:53 +0200

    Funciona v1.0

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,8 +5,21 @@ import { Component } from '@angular/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
+  numero: number = 0;
+  numSecret: number = Math.floor(Math.random() * 1000) + 1;
+  
+
   constructor() {}
+
+  suma() {
+    this.numero++;
+  }
+
+  resta() {
+    this.numero--;
+  }
 
 }
